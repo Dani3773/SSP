b7914db7299c2249b0f8bf78d6336ab5bab7838c
Igor <igorraiciky@gmail.com>
2025-10-15 14:36:46 -0300

    feat: mudanças no scripr do mapa e foto de noticias

--- src/function/mapScript.js
+++ src/function/mapScript.js
@@ -12,10 +12,10 @@ const cameraIcon = L.icon({
   popupAnchor: [0, -32]         
 });
 
-L.marker([-28.6775, -49.3698], { icon: cameraIcon })
+L.marker([-28.7069, -49.4166], { icon: cameraIcon })
   .addTo(map)
   .bindPopup(`
-    <b>Câmera - Centro de Criciúma</b><br>
+    <b>Câmera - AM - Master Hall</b><br>
     <iframe 
       width="300" 
       height="200" 
@@ -27,4 +27,159 @@ L.marker([-28.6775, -49.3698], { icon: cameraIcon })
     </iframe>
   `);
 
+  L.marker([-28.7008, -49.4102], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - CEDUP</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
 
+  L.marker([-28.6837, -49.3689], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Estádio Heriberto Hulse</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.7025, -49.4050], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Colégio SATC</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.7012, -49.4092], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - UNESC - Universidade do Extremo Sul Catarinense</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.6874, -49.3334], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Nações Shopping</b><br>
+      <iframe width="300" height="300" src="https://www.youtube.com/embed/z545k7Tcb5o" title="Périphérique Nord - Porte de la Pape" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>" 
+  `);
+
+  L.marker([-28.6868, -49.3493], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Unimed</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src=""https://www.youtube.com/embed/4y5jzMe1O5g" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.7035, -49.3602], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Parque Centenário</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.6888, -49.3801], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Prefeitura Municipal de Criciúma</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.6904, -49.3906], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - HAVAN</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.6751, -49.3692], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - Praça do Congresso</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
+
+  L.marker([-28.6789, -49.3315], { icon: cameraIcon })
+  .addTo(map)
+  .bindPopup(`
+    <b>Câmera - IFSC</b><br>
+    <iframe 
+      width="300" 
+      height="200" 
+      src="https://www.youtube.com/embed/qmE7U1YZPQA" 
+      title="Santa Monica Beach Cam powered by EXPLORE.org" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allowfullscreen>
+    </iframe>
+  `);
